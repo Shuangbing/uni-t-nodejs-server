21272017c0eaab8872d9efd0b02a8cbf4b93fed2
Shuangbing <he.shuangbing@gmail.com>
2019-05-24 14:25:30 +0900

    add send attendance

--- routes/school.js
+++ routes/school.js
@@ -22,7 +22,7 @@ router.post('/api/verify', UserMiddle, SchoolMiddle, async(req, res) => {
     })
 })
 
-router.post('/api/timetable', UserMiddle, SchoolMiddle, async(req, res) => {
+router.get('/api/timetable', UserMiddle, SchoolMiddle, async(req, res) => {
     if (!req.school_account) { return response.sendError(res, 'school account time limit') }
     const school_api = require('./school_api/'+String(req.school.apiPath))
     school_api.syncTimeTable(req.school_account.susr, req.school_account.spsw)
@@ -45,10 +45,27 @@ router.get('/api/attendance', UserMiddle, SchoolMiddle, async(req, res) => {
 router.post('/api/attendance', UserMiddle, SchoolMiddle, async(req, res) => {
     if (!req.school_account) { return response.sendError(res, 'school account time limit') }
     const school_api = require('./school_api/'+String(req.school.apiPath))
-    school_api.attendanceList(req.school_account.susr, req.school_account.spsw)
+    school_api.attendancePost(req.school_account.susr, req.school_account.spsw, req.body.attendanceCode, req.body.attendanceNo)
     .then((data)=>{
-        if(!data){ return response.sendError(res, 'get attendance list faild') }
-        response.sendSuccess( res, data, 'get attendance list success')
+        if(!data){ return response.sendError(res, 'attendance faild') }
+        switch(data){
+            case 1:
+                response.sendSuccess( res, data, 'attendance success' )
+                break
+            case 2:
+                response.sendError( res, 'already attendance' )
+                break
+        }
+    })
+})
+
+router.get('/api/grade', UserMiddle, SchoolMiddle, async(req, res) => {
+    if (!req.school_account) { return response.sendError(res, 'school account time limit') }
+    const school_api = require('./school_api/'+String(req.school.apiPath))
+    school_api.gradeQuery(req.school_account.susr, req.school_account.spsw)
+    .then((data)=>{
+        if(!data){ return response.sendError(res, 'grade query faild') }
+        response.sendSuccess( res, data, 'grade query success' )
     })
 })
 
--- routes/school_api/demo_university01.js
+++ routes/school_api/demo_university01.js
@@ -58,18 +58,26 @@ async function attendancePost(sc_user, sc_password, attendCode, attendNo) {
     })
     const data_SendAttend = qs.stringify({
         module: 'Sk',
-        action: ProcedureAcc,
+        action: 'ProcedureAcc',
         SelKamoku: attendCode,
         InpNo: attendNo
     })
     return axios.post('https://call.off.tcu.ac.jp/index.php'+'?menuname=%8Fo%90%C8&', data_Login, { responseType: 'arraybuffer' })
-    .then(() => {
+    .then((res) => {
         //if(!html.includes(sc_user)) { return false }
-        return axios.post('https://call.off.tcu.ac.jp/index.php'+'?submitButtonName=%8Fo%90%C8%93o%98%5E&', data_SendAttend, { responseType: 'arraybuffer' })
+        return axios.post('https://call.off.tcu.ac.jp/index.php'+'?submitButtonName=%8Fo%90%C8%93o%98%5E&',
+        data_SendAttend,
+        {
+            responseType: 'arraybuffer',
+            headers: {
+                Cookie: res.headers['set-cookie']
+            }
+        })
         .then((res) => {
             const html = jconv.decode(res.data, "SJIS");
-            if(!html.includes('出席を受け付けました')) { return false }
-            return true
+            if(html.includes('出席を受け付けました')) { return 1 }
+            if(html.includes('既に出席登録が行われています')) { return 2 }
+            return false
         })
     })
 }
@@ -93,16 +101,18 @@ async function syncTimeTable(sc_user, sc_password) {
             var timetable = [];
             $('tr[data-class_order]').each(function(j, item) {
                 time++;
-                $(item).find('div a').each(function(i, item) {
+                $(item).find('td[class!=schedule-table-class_order]').each(function(i, item) {
                     week = i;
                     $name = $(item).text().split("» ").pop().split(" (").shift()
                     $teacher = $(item).text().split("、")[1]
-                    timetable.push({
-                        week: week,
-                        time: time,
-                        lesson_name: $name,
-                        lesson_teacher: $teacher
-                    })
+                    if($name.length > 0) {
+                        timetable.push({
+                            week: week,
+                            time: time,
+                            lesson_name: $name,
+                            lesson_teacher: $teacher
+                        })
+                    }
                 })
             })
             return timetable
@@ -113,6 +123,32 @@ async function syncTimeTable(sc_user, sc_password) {
     })
 }
 
+async function gradeQuery(sc_user, sc_password) {
+    const jconv = require('jconv');
+    const data = qs.stringify({
+        userID: sc_user,
+        password: sc_password,
+        buttonName: 'login',
+        lang: '1'
+    })
+    return axios.post('https://websrv.tcu.ac.jp/tcu_web_v3/login.do', data)
+    .then((res) => {
+        return axios.get('https://websrv.tcu.ac.jp/tcu_web_v3/wssrlstr.do?clearAccessData=false&contenam=wssrlstr&kjnmnNo=6', {
+            headers: {
+                Cookie: res.headers['set-cookie']
+            }
+        }).then((res) => {
+            console.log(res.data)
+            const html = res.data
+            const $ = cheerio.load(html)
+            var gradeList = [];
+            return gradeList
+        })
+    })
+}
+
 module.exports.attendanceList = attendanceList
+module.exports.attendancePost = attendancePost
 module.exports.verifySchoolAccount = verifySchoolAccount
 module.exports.syncTimeTable = syncTimeTable
+module.exports.gradeQuery = gradeQuery
